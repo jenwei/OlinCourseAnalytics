89a7c63bcf659493aa81a0d6b1542b99954bb244
jenwei <jennifer.wei@students.olin.edu>
2014-05-03 12:17:50 -0400

    modified views and updated urls to fit the views

--- CeceliaDjangoStuff/projectsite/courses/urls.py
+++ CeceliaDjangoStuff/projectsite/courses/urls.py
@@ -4,12 +4,12 @@ from courses import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^individualSearch/$', views.course, name='course'),
+    url(r'^individualSearch/$', views.courseSearch, name='courseSearch'),
     #url(r'^(?P<course_id>\d+)/$', views.course, name='course'),
     #url(r'^findCourse/$',views.singleCourseSearch, name='singleCourseSearch'),
     url(r'^compare/$', views.compare, name='compare'),
-    url(r'^split/$', views.split, name='split'),
+    #url(r'^split/$', views.split, name='split'),
     url(r'^advanceSearch/$', views.doSearch, name="doSearch"),
-    url(r'^team/$', views.index, name = 'team') #replace this with its own view & create template for it too
+    url(r'^team/$', views.team, name = 'team') #replace this with its own view & create template for it too
 )
 
